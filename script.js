b3d50b299e7c790000f75ea581777393c33163e9
hckf <45421713+hckf@users.noreply.github.com>
2021-11-04 12:57:31 +0000

    Improved etch-a-sketch flexbox functionality, added dynamic size slider.

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
+"use strict";
+
 function createGrid(gridSize) {
   let divGrid = document.createElement("div");
   divGrid.className = "div-grid";
-  document.body.appendChild(divGrid);
-  let divRow;
+  divGrid.id = "grid";
+  let toolBar = document.getElementById("toolbar");
+  document.body.insertBefore(divGrid, toolBar);
+  let divCol;
   for (let i = 0; i < gridSize; i++) {
-    divRow = document.createElement("div");
-    divRow.className = "div-row";
-    divGrid.appendChild(divRow);
+    divCol = document.createElement("div");
+    divCol.className = "div-col";
+    divGrid.appendChild(divCol);
     let divCell;
     for (let j = 0; j < gridSize; j++) {
       divCell = document.createElement("div");
-      divRow.appendChild(divCell);
+      divCol.appendChild(divCell);
       divCell.className = "div-cell";
     }
   }
@@ -24,6 +28,7 @@ function gridPen() {
       ++mouseDown;
     }
   };
+
   document.body.onmouseup = function () {
     if (mouseDown == 1) {
       --mouseDown;
@@ -31,17 +36,16 @@ function gridPen() {
   };
 
   let divs = Array.from(document.getElementsByClassName("div-cell"));
+
   for (const div of divs) {
     div.addEventListener("mousemove", (e) => {
-      if ((div.style.backgroundColor = "transparent" && mouseDown == 1)) {
-        div.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+      if (penColor == "rainbow" && mouseDown == 1) {
+        div.style.backgroundColor = rainbowPen();
+      } else if (penColor == "black" && mouseDown == 1) {
+        div.style.backgroundColor = "rgb(0, 0, 0)";
+      } else if (penColor == "eraser" && mouseDown == 1) {
+        div.style.backgroundColor = "transparent";
       }
-      div.addEventListener("click", (e) => {
-        if (div.style.backgroundColor != "transparent") {
-          div.style.backgroundColor = "transparent";
-          // div.style.backgroundColor = `transparent`;
-        }
-      });
     });
   }
 }
@@ -57,9 +61,61 @@ function randomRGB() {
   return Math.floor(Math.random() * 255);
 }
 
-window.onload = () => {
-  createGrid(36);
+function rainbowPen() {
+  return `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+}
+
+function changeToRainbow() {
+  penColor = "rainbow";
+}
+
+function changeToBlack() {
+  penColor = "black";
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+// window.onload = () => {
+let penColor = "rainbow";
+createGrid(16);
+gridPen();
+let clearButton = document.getElementById("clear-button");
+clearButton.addEventListener("click", gridClear);
+let blackButton = document.getElementById("black-button");
+blackButton.addEventListener("click", changeToBlack);
+let rainbowButton = document.getElementById("rainbow-button");
+rainbowButton.addEventListener("click", changeToRainbow);
+
+let slider = document.getElementById("grid-size-slider");
+let number = document.getElementById("grid-size-number");
+
+slider.oninput = function () {
+  // let oldGrid = document.querySelector("#container");
+  // removeAllChildNodes(container);
+
+  let oldGrid = document.getElementById("grid");
+  oldGrid.innerHTML = "";
+  oldGrid.remove();
+
+  number.value = this.value;
+  createGrid(number.value);
   gridPen();
-  let clearButton = document.getElementById("clear-button");
-  clearButton.addEventListener("click", gridClear);
 };
+
+number.oninput = function () {
+  // let oldGrid = document.querySelector("#container");
+  // removeAllChildNodes(container);
+
+  let oldGrid = document.getElementById("grid");
+  oldGrid.innerHTML = "";
+  oldGrid.remove();
+
+  number.value = this.value;
+  createGrid(number.value);
+  gridPen();
+};
+// };
